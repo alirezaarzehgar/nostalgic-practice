96d832912b6d536915231d5af6fb5b9b5e7d4fe0
alirezaarzehgar <alirezaarzehgar82@gmail.com>
2025-05-18 23:43:28 +0330

    change text colors
    Signed-off-by: alirezaarzehgar <alirezaarzehgar82@gmail.com>

--- main.c
+++ main.c
@@ -10,9 +10,9 @@ int main() {
 
   for (size_t i = 0; getline(&line, &n, stdin) > -1; i++) {
     if (!grammar_check(line, n))
-      printf("❌ \033[31m%zu: %s\033[0m", i, line);
+      printf("❌ \033[91m%zu: %s\033[0m", i, line);
     else
-      printf("✅ \033[36m%zu: %s\033[0m", i, line);
+      printf("✅ \033[32m%zu: %s\033[0m", i, line);
 
     free(line);
     line = NULL;
